87ed7d8595b739747558ff2194c843b532afe729
Mekkinen <meo.ekroos@gmail.com>
2017-08-15 21:40:21 +0300

    test2

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $('#dropdown').prepend('');
         var w = element.width();
         if ($(this).hasClass('has-sub'))
         {
-        	leftPos = element.position().left + w/2 - 4;
+        	leftPos = element.position().left + w/2 - 20;
         }
         else {
         	leftPos = element.position().left + w/2 - 6;
